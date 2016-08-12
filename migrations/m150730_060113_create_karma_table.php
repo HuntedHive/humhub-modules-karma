49b1d284c5e04392a45ea654110926eed33545a9
Den Volkov <volkovdenya@gmail.com>
2016-08-12 09:49:51 +0300

    add exists of create table

--- migrations/m150730_060113_create_karma_table.php
+++ migrations/m150730_060113_create_karma_table.php
@@ -6,12 +6,14 @@ class m150730_060113_create_karma_table extends Migration
 {
 	public function up()
 	{
-        $this->createTable('karma', array(
-            'id' => 'pk',
-            'name' => 'varchar(100) NOT NULL',
-            'points' => 'int(11) NOT NULL',
-            'description' => 'TEXT',
-        ), '');
+		if(!\Yii::$app->db->schema->getTableSchema("karma")) {
+			$this->createTable('karma', array(
+				'id' => 'pk',
+				'name' => 'varchar(100) NOT NULL',
+				'points' => 'int(11) NOT NULL',
+				'description' => 'TEXT',
+			), '');
+		}
 	}
 
 	public function down()
